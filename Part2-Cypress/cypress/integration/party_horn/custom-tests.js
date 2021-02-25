64789827e0268965258895ca84470e0f2cad3849
ethanh6 <38364796+ethanh6@users.noreply.github.com>
2021-02-25 21:41:20 +0800

    update

--- Part2-Cypress/cypress/integration/party_horn/custom-tests.js
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -6,4 +6,35 @@ describe('Party Horn Tests', () => {
   it('First Test', () => {
     expect(true).to.equal(true);
   });
+
+  it("Slider changes when volume input changes", () => {
+    cy.get("#volume-number").clear().type('75');
+    cy.get("#volume-slider").then(($el) => {
+      expect($el).to.have.value(75);
+    });
+  });
+
+  it("Input changes when volume slider changes", () => {
+    cy.get("#volume-slider").invoke("val", 33).trigger("input");
+    cy.get("#volume-number").then(($el) => {
+      expect($el).to.have.value(33);
+    });
+  });
+
+  it("Horn sound volume changes when volume slider change", () => {
+    cy.get("#volume-slider").invoke("val", 33).trigger("input");
+    cy.get("#horn-sound").then(($el) => {
+      expect($el).to.have.prop("volume", 0.33);
+    });
+  });
+
+  // -------- -------- -------- -------- 
+
+  it("Image changes when party horn selected", () => {
+    cy.get("#radio-party-horn").invoke().trigger("input");
+    cy.get("#sound-image").then(($el) => {
+      expect($el).to.have.prop("src", "./assets/media/images/party-horn.svg");
+    });
+  });
+
 });
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Cypress and Jest weren't working too well together
 so we've separated the two out in different parts
+
+## Name: Ethan Huang
+## PID: A16080153
